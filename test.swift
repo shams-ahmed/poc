9415df6cc1d0037355e88015096162864bbcbf93
Shams Ahmed <shamsahmed@me.com>
2020-05-19 10:48:05 +0100

    file permission

--- test.swift
+++ test.swift
@@ -1,3 +1,17 @@
 #!/usr/bin/env xcrun swift
 
+import Foundation
+
 print("Hello Shams from test.swift file!")
+
+let process = ProcessInfo.processInfo
+let environment = process.environment
+let arguments = process.arguments
+
+print("Environment")
+environment.forEach { print($0) }
+
+print("")
+
+print("Arguments")
+arguments.forEach { print($0) }
